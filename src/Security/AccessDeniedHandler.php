7d0bb2a6f7c007016487643eefc4c25119e1e286
Youssef-AH <qpinoza@gmail.com>
2020-06-14 13:17:07 +0100

    Added custom access denied handler

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Security;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $content = '<html><head><title>Access Denied</title></head><body>
+        <h1>403</h1>
+        <h1>You are not authorized to access this resource</h1>
+        </body></html>';
+        return new Response($content, 403);
+    }
+}
