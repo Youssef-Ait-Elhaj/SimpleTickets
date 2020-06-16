b75f92516c5e57f7ddc16fceaca6f51ab0193519
Youssef-AH <qpinoza@gmail.com>
2020-06-16 13:02:43 +0100

    Added Client's functionality

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -8,6 +8,7 @@ use App\Form\TicketFormType;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -22,7 +23,7 @@ class TicketController extends AbstractController
     }
 
     /**
-     * @Route("new", name="ticket")
+     * @Route("/tickets/new", name="ticket_create")
      */
     public function createTicket(Request $request, ObjectManager $manager)
     {
@@ -41,8 +42,52 @@ class TicketController extends AbstractController
             $manager->persist($ticket);
             $manager->flush();
         }
-        return $this->render('ticket/newticket.html.twig', [
+        return $this->render('ticket/new_ticket.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/tickets/edit/{id}", name="ticket_edit")
+     */
+    public function editTicket(Ticket $ticket, Request $request, ObjectManager $manager) {
+        $form = $this->createForm(TicketFormType::class, $ticket);
+        $form->handleRequest($request);
+//
+//        $entityManager = $this->getDoctrine()->getManager();
+//        $ticket = $entityManager->getRepository(Ticket::class)->find($id);
+
+        if (!$ticket) {
+            throw $this->createNotFoundException('No Ticket found for id ' . $id);
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ticket);
+            $manager->flush();
+        }
+
+        return $this->render('ticket/edit_ticket.html.twig', [
+            'form' => $form->createView(),
+            'ticket' => $ticket
+        ]);
+    }
+
+    /**
+     * @Route("/tickets/show/{id}", name="ticket_show")
+     */
+    public function showTicket(Ticket $ticket) {
+        return $this->render('ticket/show_ticket.html.twig', [
+            'ticket' => $ticket
+        ]);
+    }
+
+    /**
+     * @Route("/tickets/delete/{id}", name="ticket_delete")
+     */
+    public function deleteTicket(Ticket $ticket, ObjectManager $manager) {
+        $manager->remove($ticket);
+        $manager->flush();
+        $res = '<html><body><h1>DELETED SUCCESSFULLY</h1></body></html>';
+        return new Response($res, 202);
     }
 }
